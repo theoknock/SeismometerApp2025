0c916d5f9d6f91cb79f8a1395112888660b0225b
James Alan Bush <theoknock@gmail.com>
2025-04-24 02:18:48 -0400

    Initial Commit

---
+++ SeismometerApp2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeismometerApp2025
+//
+//  Created by Xcode Developer on 4/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SeismometerApp2025/SeismometerApp2025App.swift
@@ -0,0 +1,17 @@
+//
+//  SeismometerApp2025App.swift
+//  SeismometerApp2025
+//
+//  Created by Xcode Developer on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SeismometerApp2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
